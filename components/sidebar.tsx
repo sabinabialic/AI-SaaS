840b8789475579971ecc927483a8436ec7442a8b
Sabina Bialic <sabina.bialic@sap.com>
2024-02-26 15:19:20 -0500

    Working on the sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Code2Icon, ImagesIcon, LayoutDashboard, MessageCircle, Music2Icon, SettingsIcon, VideoIcon } from "lucide-react";
@@ -55,6 +56,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+    const pathname = usePathname();
+    
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -72,8 +75,8 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer 
-                                hover:text=white hover:bg-white/10 rounded-lg transition">
+                            className={cn ("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text=white hover:bg-white/10 rounded-lg transition", 
+                            pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
                             
                             <div className="flex items-center items-1">
                                 <route.icon className={cn("h-4 w-4 mr-4", route.color)} />
